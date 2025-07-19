f790aa57c070aab88655c78ebbbb9ddf51c2e003
Wilian Cesar <wilcesar@live.com>
2025-07-19 15:28:33 -0300

    Alimentando modelos

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -11,12 +11,18 @@ export class CriarPensamentoComponent implements OnInit {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo: ''
+    modelo: 'modelo1'
   }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  criarPensamento(){
+    alert("Novo pensamento criado!")
+  }
+  cancelar(){
+    alert("Pensamento cancelado")
   }
 
 }
