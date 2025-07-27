57061e3627437d7c5dbc95167b9f2004d175a1d6
Wilian Cesar <wilcesar@live.com>
2025-07-27 12:38:02 -0300

    Adicionando validação no editar

--- memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Pensamento } from './../pensamento';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -10,6 +11,7 @@ import { Pensamento } from './../pensamento';
 })
 export class EditarPensamentoComponent implements OnInit {
 
+  formulario!: FormGroup;
   pensamento: Pensamento ={
     id:0,
     conteudo: '',
@@ -19,7 +21,8 @@ export class EditarPensamentoComponent implements OnInit {
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -27,13 +30,34 @@ export class EditarPensamentoComponent implements OnInit {
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento)=>{
       this.pensamento = pensamento
     })
+    this.formulario = this.formBuilder.group({
+          conteudo: ['',Validators.compose([
+            Validators.required,
+            Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+          autoria:['',Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+        ])],
+          modelo:['modelo1']
+        })
   }
   editarPensamento(){
-    this.service.editar(this.pensamento).subscribe(()=>{
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors);
+    if(this.formulario.valid){
+      this.service.editar(this.pensamento).subscribe(()=>{
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
   cancelar(){
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return "botao"
+    }
+    else return "botao__desabilitado"
   }
 }
